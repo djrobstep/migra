a296420cdcedbfdac95f0fd2f775dffa5b2bdbec
Alexey Zhokhov <alexey@zhokhov.com>
2018-08-05 22:18:42 +0800

    Log docker command only if env is enabled.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,10 @@
 set -e
 
 if [ "$1" = 'migra' ]; then
-	migra_command="$@"
-	echo "${migra_command}"
+	if [ "${MIGRA_LOG_COMMAND}" = 'true' ]; then
+		migra_command="$@"
+		echo "${migra_command}"
+	fi
 fi
 
 exec "$@"
