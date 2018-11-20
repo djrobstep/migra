56e24d4b8add5190c9a6bc655a6091ebc8100655
Robert Lechte <robertlechte@gmail.com>
2018-11-20 17:55:00 +1100

    fix collation tests

--- tests/FIXTURES/collations/a.sql
+++ tests/FIXTURES/collations/a.sql
@@ -1,8 +1,8 @@
-CREATE COLLATION german FROM "'en-u-kn-true'";
+CREATE COLLATION posix FROM "POSIX";
 
 create table t(
   a text,
-  b text collate german
+  b text collate posix
 );
 
 CREATE COLLATION numeric (provider = icu, locale = 'en-u-kn-true');
--- tests/FIXTURES/collations/expected.sql
+++ tests/FIXTURES/collations/expected.sql
@@ -2,4 +2,4 @@ alter table "public"."t" add column "c" text collate "numeric";
 
 alter table "public"."t" alter column "b" set data type text collate "numeric";
 
-drop collation if exists "public"."german";
+drop collation if exists "public"."posix";
