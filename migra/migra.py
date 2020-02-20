8111f31a75c99d72afb5b5edb533f93d1b0675e7
atolmachev <atolmachev@cfmc.ru>
2020-02-20 18:38:15 +0300

    support for comments
    (cherry picked from commit 0343f1eb43daca3991e65745683011c4af8a3034)

--- migra/migra.py
+++ migra/migra.py
@@ -93,6 +93,7 @@ class Migration(object):
         self.add(self.changes.enums(creations_only=True, modifications=False))
 
         self.add(self.changes.sequences(creations_only=True))
+        self.add(self.changes.comments(drops_only=True, modifications=False))
         self.add(self.changes.triggers(drops_only=True))
         self.add(self.changes.rlspolicies(drops_only=True))
         if privileges:
@@ -121,6 +122,7 @@ class Migration(object):
             self.add(self.changes.privileges(creations_only=True))
         self.add(self.changes.rlspolicies(creations_only=True))
         self.add(self.changes.triggers(creations_only=True))
+        self.add(self.changes.comments(creations_only=True))
         self.add(self.changes.collations(drops_only=True))
         self.add(self.changes.schemas(drops_only=True))
 
