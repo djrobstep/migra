e514bd7cfa581bc323bc75e7a33be5720c13bc66
Jason Giancono <jgiancono@actu.org.au>
2020-10-07 10:31:52 +1100

    add exclude schema option

--- migra/migra.py
+++ migra/migra.py
@@ -12,28 +12,39 @@ class Migration(object):
     The main class of migra
     """
 
-    def __init__(self, x_from, x_target, schema=None):
+    def __init__(self, x_from, x_target, schema=None, exclude_schema=None):
         self.statements = Statements()
         self.changes = Changes(None, None)
+        if schema and exclude_schema:
+            raise ValueError("You cannot have both a schema and excluded schema")
         self.schema = schema
+        self.exclude_schema = exclude_schema
         if isinstance(x_from, DBInspector):
             self.changes.i_from = x_from
         else:
-            self.changes.i_from = get_inspector(x_from, schema=schema)
+            self.changes.i_from = get_inspector(
+                x_from, schema=schema, exclude_schema=exclude_schema
+            )
             if x_from:
                 self.s_from = x_from
         if isinstance(x_target, DBInspector):
             self.changes.i_target = x_target
         else:
-            self.changes.i_target = get_inspector(x_target, schema=schema)
+            self.changes.i_target = get_inspector(
+                x_target, schema=schema, exclude_schema=exclude_schema
+            )
             if x_target:
                 self.s_target = x_target
 
     def inspect_from(self):
-        self.changes.i_from = get_inspector(self.s_from, schema=self.schema)
+        self.changes.i_from = get_inspector(
+            self.s_from, schema=self.schema, exclude_schema=self.exclude_schema
+        )
 
     def inspect_target(self):
-        self.changes.i_target = get_inspector(self.s_target, schema=self.schema)
+        self.changes.i_target = get_inspector(
+            self.s_target, schema=self.schema, exclude_schema=self.exclude_schema
+        )
 
     def clear(self):
         self.statements = Statements()
@@ -41,7 +52,9 @@ class Migration(object):
     def apply(self):
         for stmt in self.statements:
             raw_execute(self.s_from, stmt)
-        self.changes.i_from = get_inspector(self.s_from, schema=self.schema)
+        self.changes.i_from = get_inspector(
+            self.s_from, schema=self.schema, exclude_schema=self.exclude_schema
+        )
         safety_on = self.statements.safe
         self.clear()
         self.set_safety(safety_on)
---
+++ tests/FIXTURES/excludeschema/a.sql
@@ -0,0 +1,11 @@
+create schema excludedschema;
+
+create table excludedschema.t(id uuid, value text);
+
+create schema schema1;
+
+create table schema1.t(id uuid, value text);
+
+create schema schema2;
+
+create table schema2.t(id uuid, value text);
