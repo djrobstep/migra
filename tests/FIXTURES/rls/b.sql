a672272a7d81eff1cf78bff1d89da7c0bd37d0cb
Aaron Merryman <amerryma@asu.edu>
2022-02-04 10:42:19 -0700

    Using latest version of schemainspect, this will now properly put parens around policies using `with check` (#5)

--- tests/FIXTURES/rls/b.sql
+++ tests/FIXTURES/rls/b.sql
@@ -1,9 +1,18 @@
 CREATE TABLE accounts (manager text, company text, contact_email text);
 
+CREATE FUNCTION is_manager(manager text, out success boolean) returns boolean as $$
+begin
+    select manager = current_user into success;
+end;
+$$ language plpgsql stable;
+
 ALTER TABLE accounts ENABLE ROW LEVEL SECURITY;
 
-CREATE POLICY account_managers ON accounts as restrictive TO schemainspect_test_role
+CREATE POLICY account_managers_update ON accounts as restrictive FOR UPDATE TO schemainspect_test_role
     USING (manager = current_user);
+
+CREATE POLICY account_managers_insert ON accounts as restrictive FOR INSERT TO schemainspect_test_role
+    WITH CHECK (is_manager(manager));
 
 CREATE TABLE accounts2 (manager text, company text, contact_email text);
 
--- tests/FIXTURES/rls/expected.sql
+++ tests/FIXTURES/rls/expected.sql
@@ -1,11 +1,22 @@
-drop policy if exists "account_managers" on "public"."accounts";
+drop policy if exists "account_managers_insert" on "public"."accounts";
+
+drop policy if exists "account_managers_update" on "public"."accounts";
 
 alter table "public"."accounts2" enable row level security;
 
-drop policy if exists "account_managers" on "public"."accounts";
-create policy "account_managers"
+drop policy if exists "account_managers_insert" on "public"."accounts";
+create policy "account_managers_insert"
 on "public"."accounts"
 as restrictive
-for all
+for insert
+to schemainspect_test_role
+with check (is_manager(manager));
+
+
+drop policy if exists "account_managers_update" on "public"."accounts";
+create policy "account_managers_update"
+on "public"."accounts"
+as restrictive
+for update
 to schemainspect_test_role
 using ((manager = CURRENT_USER));
