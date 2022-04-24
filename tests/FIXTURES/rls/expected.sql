d4833bc8d0c154b09335ae28d4bae3718ac57001
Aaron Merryman <amerryman@mindspace.net>
2022-04-24 16:51:23 -0700

    Fixed tests and updated lock file

--- tests/FIXTURES/rls/expected.sql
+++ tests/FIXTURES/rls/expected.sql
@@ -1,10 +1,22 @@
-drop policy "account_managers" on "public"."accounts";
+drop policy "account_managers_insert" on "public"."accounts";
+
+drop policy "account_managers_update" on "public"."accounts";
 
 alter table "public"."accounts2" enable row level security;
 
-create policy "account_managers"
+create policy "account_managers_insert"
 on "public"."accounts"
 as restrictive
-for all
+for insert
+to schemainspect_test_role
+with check (is_manager(manager));
+
+
+create policy "account_managers_update"
+on "public"."accounts"
+as restrictive
+for update
 to schemainspect_test_role
 using ((manager = CURRENT_USER));
+
+
